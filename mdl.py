06566d49de8a6a2873cf38a3562d4061e9bd7364
mmiah00 <mmiah00@stuy.edu>
2020-06-09 18:12:29 +0100

    editted mesh command in mdl

--- mdl.py
+++ mdl.py
@@ -336,15 +336,24 @@ def p_command_mesh(p):
                | MESH CO TEXT SYMBOL
                | MESH SYMBOL CO TEXT SYMBOL"""
     cmd = {'op':p[1], 'args' : [], 'cs':None, 'constants':None}
-    arg_start = 2
-    if isinstance(p[2], str):
+    if p[2][0] == ':':
+        cmd['args'].append (p[3])
+        if len (p) == 5:
+            cmd['cs'] = p[4]
+    else:
         cmd['constants'] = p[2]
-        arg_start+= 1
-    cmd['args'].append(p[arg_start])
-    if len(p) == 4 and isinstance(p[3], str):
-        cmd['cs'] = p[3]
-    if len(p) == 5 and isinstance(p[4], str):
-        cmd['cs'] = p[4]
+        cmd['args'].append (p[4])
+        if len (p) == 6:
+            cmd['cs'] = p[5]
+    # arg_start = 2
+    # if isinstance(p[2], str):
+    #     cmd['constants'] = p[2]
+    #     arg_start+= 1
+    # cmd['args'].append(p[arg_start])
+    # if len(p) == 4 and isinstance(p[3], str):
+    #     cmd['cs'] = p[3]
+    # if len(p) == 5 and isinstance(p[4], str):
+    #     cmd['cs'] = p[4]
     commands.append(cmd)
 
 def p_save_knobs(p):

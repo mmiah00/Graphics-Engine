a2e4b0cd138d12e9f976ea79892205bda163d2d0
mmiah00 <maishm357@gmail.com>
2020-06-11 22:50:53 -0400

    .

--- mdl.py
+++ mdl.py
@@ -330,6 +330,22 @@ def p_command_generate_rayfiles(p):
     "command : GENERATE_RAYFILES"
     commands.append({'op':p[1], 'args':None})
 
+# def p_command_mesh(p):
+#     """command : MESH CO TEXT
+#                | MESH SYMBOL CO TEXT
+#                | MESH CO TEXT SYMBOL
+#                | MESH SYMBOL CO TEXT SYMBOL"""
+#     cmd = {'op':p[1], 'args' : [], 'cs':None, 'constants':None}
+#     arg_start = 2
+#     if isinstance(p[2], str):
+#         cmd['constants'] = p[2]
+#         arg_start+= 1
+#     cmd['args'].append(p[arg_start])
+#     if len(p) == 4 and isinstance(p[3], str):
+#         cmd['cs'] = p[3]
+#     if len(p) == 5 and isinstance(p[4], str):
+#         cmd['cs'] = p[4]
+#     commands.append(cmd)
 def p_command_mesh(p):
     """command : MESH CO TEXT
                | MESH SYMBOL CO TEXT
